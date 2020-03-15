38ab5f080b5dfafc0e76cf9502bccc9c01f35d8f
amedlin <4089144+amedlin@users.noreply.github.com>
2020-03-15 16:55:52 +1100

    Fixed the math, I believe it is now working as intended.

--- model.py
+++ model.py
@@ -3,14 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
-P0 = 10000.0
+P0 = 25000000.0
 N0 = 1.0
 U0 = P0 - N0
 M0 = 0.0
 D0 = 0.0
 K0 = 0.0
 
-T = 365.0
+T = 180.0
 dt = 1.0
 alpha = 0.22
 f = 0.01
@@ -36,36 +36,45 @@ Nint = interp1d(t, N, copy=False, bounds_error=False, fill_value=0.0)
 for i in range(1, L):
     _t = t[i]
     # N[i] = max(N[i-1] + ((alpha*(P[i-1] - M[i-1])/P[i-1])*Nint(_t - ti) - Nint(_t - tau))*dt, 0.0)
-    growthN[i] = ((alpha*max(P[i-1] - N[i-1] - M[i-1], 0.0)/P[i-1])*Nint(_t - ti))*dt
+    growthN[i] = ((alpha*U[i-1]/P[i-1])*Nint(_t - ti))*dt
     assert growthN[i] >= 0.0
-    N[i] = max(N[i-1] + growthN[i] - (Nint(_t - tau) - Nint(_t - tau - dt)), 0.0)
-    Nint = interp1d(t, N, copy=False, bounds_error=False, fill_value=0.0)
     growthNint = interp1d(t, growthN, copy=False, bounds_error=False, fill_value=0.0)
-    dD = f*growthNint(_t - tau)
+    resolved = growthNint(_t - tau)
+    N[i] = max(N[i-1] + growthN[i] - resolved, 0.0)
+    Nint = interp1d(t, N, copy=False, bounds_error=False, fill_value=0.0)
+    dD = f*resolved
     D[i] = D[i-1] + dD
-    M[i] = M[i-1] + (1 - f)*growthNint(_t - tau)
+    M[i] = M[i-1] + (1 - f)*resolved
     P[i] = P[i-1] - dD
+    assert np.isclose(P[i] + D[i], P0)
     assert N[i] <= P[i]
     assert M[i] <= P[i]
     K[i] = s*Nint(_t - ti)
-    U[i] = P0 - N[i] - M[i]
-    assert np.isclose(P[i] + D[i], P0)
+    U[i] = P[i] - N[i] - M[i]
+    assert U[i] >= 0.0
     print("Day: {:.2f}, Cumulative cases: {:.2f}, Recovered cases: {:.2f}, Deaths: {:.2f}, Pop: {:.2f}".format(
         _t, N[i] + M[i] + D[i], M[i], D[i], P[i]))
 # end for
+assert np.isclose(P[-1] + D[-1], P0)
+assert np.isclose(P[-1], U[-1] + N[-1] + M[-1])
 
-all = np.vstack((N, K, D, M))
+ir = (N[-1] + M[-1] + D[-1])/P0
+print("Infection rate: {}".format(ir))
+
+# Compute number  of cases we don't know
+CI = N - K
+
+all = np.floor(np.vstack((N, K, D, M, CI)))
 plt.figure(figsize=(16,9))
-plt.plot(t, all.T, 'x--', linewidth=2.0, alpha=0.7)
-plt.legend(['Actual active cases', 'Known active cases', 'Deaths', 'Recovered'])
+plt.plot(t, all.T, '--', linewidth=2.0, alpha=0.6)
+leg_labels = ['Actual active cases', 'Known active cases', 'Deaths', 'Recovered', 'Case ignorance']
+plt.legend(leg_labels)
 plt.grid(linestyle=':', color='#80808080')
 plt.show()
 
-all[(all == 0.0)] = np.nan
+all[(all <= 0.0)] = np.nan
 plt.figure(figsize=(16,9))
-plt.semilogy(t, all.T, 'x--', linewidth=2.0, alpha=0.7)
-plt.legend(['Actual active cases', 'Known active cases', 'Deaths', 'Recovered'])
+plt.semilogy(t, all.T, '--', linewidth=2.0, alpha=0.6)
+plt.legend(leg_labels)
 plt.grid(linestyle=':', color='#80808080')
 plt.show()
-
-pass
